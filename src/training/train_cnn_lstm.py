21112a99aa84c9dcac831d5efa72c9149dd1493d
Adderalfox <achetia8031@gmail.com>
2025-09-20 20:15:47 +0530

    Training scripts for both lstm and cnn_lstm locked in

--- src/training/train_cnn_lstm.py
+++ src/training/train_cnn_lstm.py
@@ -1,12 +1,12 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from src.models.lstm_generator import LSTMGenerator
-from torch.utils.data import DataLoader, Dataset
+from torch.utils.data import Dataset, DataLoader
 from tqdm import tqdm
 import os
 import json
 from src.data_pipeline.preprocess import preprocess_dataset, BYTE_VOCAB_SIZE
+from model import PhishModel
 import mlflow
 import mlflow.pytorch
 
@@ -14,27 +14,26 @@ mlflow.set_tracking_uri("file:./mlruns")
 mlflow.set_experiment("Phishing Detection")
 
 # ========== Config ==========
-CSV_PATH = 'data/processed/Phishing_dataset01.csv'
-ARTIFACTS_PATH = 'src/artifacts/'
-MODEL_SAVE_DIR = 'src/saved_models/'
-BATCH_SIZE = 16
-EPOCHS = 20
+CSV_PATH = '../../data/processed/Phishing_Dataset01.csv'
+ARTIFACTS_PATH = '../../artifacts/'
+MODEL_SAVE_DIR = '../saved_models/'
+BATCH_SIZE = 32
+EPOCHS = 10
 LEARNING_RATE = 0.0005
 DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
+# ========== Dataset Class ==========
 class PhishingDataset(Dataset):
-    def __init__(self, sequences):
-        sequences = torch.tensor(sequences, dtype=torch.long)
-        self.X = torch.tensor(sequences[:, :-1], dtype=torch.long)  # all but last char
-        self.y = torch.tensor(sequences[:, 1:], dtype=torch.long)   # all but first char
+    def __init__(self, X, y):
+        self.X = torch.tensor(X, dtype=torch.long)
+        self.y = torch.tensor(y, dtype=torch.long)
 
     def __len__(self):
         return len(self.X)
-
+    
     def __getitem__(self, idx):
         return self.X[idx], self.y[idx]
-
-
+    
 # ========== Train Function ==========  
 def train_one_epoch(model, dataloader, optimizer, loss_fn):
     model.train()
@@ -44,12 +43,8 @@ def train_one_epoch(model, dataloader, optimizer, loss_fn):
         inputs, labels = inputs.to(DEVICE), labels.to(DEVICE)
 
         optimizer.zero_grad()
-        outputs, _ = model(inputs)
-        loss = loss_fn(
-            outputs.reshape(-1, outputs.size(-1)),
-            labels.reshape(-1)
-        )
-
+        outputs = model(inputs)
+        loss = loss_fn(outputs, labels)
         loss.backward()
         optimizer.step()
 
@@ -58,28 +53,22 @@ def train_one_epoch(model, dataloader, optimizer, loss_fn):
     return total_loss / len(dataloader)
 
 # ========== Evaluation Function ==========
-def evaluate(model, dataloader, loss_fn):
+def evaluate(model, dataloader):
     model.eval()
-    total_loss = 0
-    total_tokens = 0
+    correct = 0
+    total = 0
 
     with torch.no_grad():
         for inputs, labels in tqdm(dataloader, desc='Validating'):
             inputs, labels = inputs.to(DEVICE), labels.to(DEVICE)
 
-            outputs, _ = model(inputs)
-            loss = loss_fn(
-                outputs.reshape(-1, outputs.size(-1)),
-                labels.reshape(-1)
-            )
+            outputs = model(inputs)
+            preds = torch.argmax(outputs, dim=1)
 
-            total_loss += loss.item() * labels.numel()
-            total_tokens += labels.numel() 
+            correct += (preds == labels).sum().item()
+            total += labels.size(0)
 
-    avg_loss = total_loss / total_tokens
-    perplexity = torch.exp(torch.tensor(avg_loss))
-
-    return avg_loss, perplexity.item()
+    return correct / total
 
 # ========== Main Function ==========
 def main():
@@ -92,39 +81,39 @@ def main():
         mlflow.set_tag("dataset", CSV_PATH)
 
         print('Preprocessing...')
-        X_train, X_val, y_train, y_val = preprocess_dataset(CSV_PATH, preprocess_file_name= "preprocess_meta_lstm", mode="train", save_path=ARTIFACTS_PATH)
+        X_train, X_val, y_train, y_val = preprocess_dataset(CSV_PATH, preprocess_file_name= "preprocess_meta_cnn_bilstm", mode="train", save_path=ARTIFACTS_PATH)
 
-        train_dataset = PhishingDataset(X_train)
-        val_dataset = PhishingDataset(X_val)
+        train_dataset = PhishingDataset(X_train, y_train)
+        val_dataset = PhishingDataset(X_val, y_val)
 
         train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
         val_loader = DataLoader(val_dataset, batch_size=BATCH_SIZE, shuffle=True)
 
-        model = LSTMGenerator(vocab_size=BYTE_VOCAB_SIZE).to(DEVICE)
+        model = PhishModel(vocab_size=BYTE_VOCAB_SIZE).to(DEVICE)
         loss_fn = nn.CrossEntropyLoss()
         optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE)
 
         os.makedirs(MODEL_SAVE_DIR, exist_ok=True)
+        best_accuracy = 0.0
 
         for epoch in range(EPOCHS):
             print(f'\nEpoch {epoch+1}/{EPOCHS}')
             train_loss = train_one_epoch(model, train_loader, optimizer, loss_fn)
             print(f'\nTraining Loss: {train_loss:.4f}')
 
-            val_loss, val_perplexity = evaluate(model, val_loader, loss_fn)
-            print(f'\nValidation Loss: {val_loss:.4f}, Validation Perplexity: {val_perplexity:.4f}')
+            val_accuracy = evaluate(model, val_loader)
+            print(f'\nValidation Accuracy: {val_accuracy:.4f}')
 
-            mlflow.log_metric("val_loss", val_loss, step=epoch)
-            mlflow.log_metric("val_perplexity", val_perplexity, step=epoch)
+            mlflow.log_metric("train_loss", train_loss, step=epoch)
+            mlflow.log_metric("val_accuracy", val_accuracy, step=epoch)
 
-            best_perplexity = float("inf")
-            if val_perplexity < best_perplexity:
-                best_perplexity = val_perplexity
-                checkpoint_path = os.path.join(MODEL_SAVE_DIR, 'best_lstm_model.pt')
+            if val_accuracy > best_accuracy:
+                best_accuracy = val_accuracy
+                checkpoint_path = os.path.join(MODEL_SAVE_DIR, 'best_model_4.pt')
                 torch.save(model.state_dict(), checkpoint_path)
                 print('The model has been saved!')
         
-        mlflow.pytorch.log_model(model, name="LSTMGenerator")
+        mlflow.pytorch.log_model(model, name="CNN")
         print('Training complete.')
 
 if __name__ == '__main__':
